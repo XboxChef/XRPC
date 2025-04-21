d16ae9b7d4407ddf751fe04f8346f11268925304
Serenity <34622204+oSerenity@users.noreply.github.com>
2025-04-20 20:28:36 -0700

    Update XRPC.cs

--- Backup/XRPCLib/XRPC.cs
+++ Backup/XRPCLib/XRPC.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: XRPCLib.XRPC
-// Assembly: XRPC, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 76786C01-8B8F-460F-885C-89B2A0240B23
-// Assembly location: C:\Users\Serenity\Desktop\XRPC.dll
-
 using System;
 using System.Text;
 using System.Threading;
